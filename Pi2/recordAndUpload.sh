e4fd82e9da8f6cb75a515f67a99ce5f768fb604a
carneyaj <50503055+carneyaj@users.noreply.github.com>
2021-05-24 15:11:19 -0400

    Update recordAndUpload.sh

--- Pi2/recordAndUpload.sh
+++ Pi2/recordAndUpload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=$(date +"%Y-%m-%d_%H:%M:%S")
-arecord -D plughw:0 -d 60 -c 2 -r 48000 -f S32_LE -t wav /home/pi/recordings/tempwav/$DATE.wav
+arecord -D plughw:0 -d 30 -c 2 -r 48000 -f S32_LE -t wav /home/pi/recordings/tempwav/$DATE.wav
 ffmpeg -i /home/pi/recordings/tempwav/$DATE.wav -acodec flac -sample_fmt s16 /home/pi/recordings/flacs/pi2_$DATE.flac
 rm /home/pi/recordings/tempwav/$DATE.wav
 rclone move /home/pi/recordings/flacs/pi2_$DATE.flac remote:recordings/pi2
